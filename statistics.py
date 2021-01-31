994c625cd4e70dd264f88e6253297647cf55f824
Janne Määttä <calm+github@iki.fi>
2021-01-31 17:27:42 +0200

    Add login to statistics, allow EV statistics to be missing

--- statistics.py
+++ statistics.py
@@ -15,11 +15,16 @@
 Get, parse and print driving statistics from MyT API
 """
 import argparse
+import logging
 import sys
 
 import pendulum
 
 from tojota import Myt
+
+logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s: %(message)s')
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 
 def parse_args():
@@ -48,27 +53,61 @@ def parse_daily_driving_statistics(myt, data):
         day_of_year = f"{item['bucket']['dayOfYear']:03d}"
         date = pendulum.from_format(f'{year} {day_of_year}', 'YYYY DDDD', tz=myt.config_data['timezone'])
         data = item['data']
-        print('{}: EV: {:.1f}/{:.1f} km, {:.0f}%, avg speed: {:.0f} km/h, max speed: {:.0f} km/h'.
-              format(date.format('YYYY-MM-DD'), data['evDistanceInKm'],
-                     data['totalDistanceInKm'], data['evDistancePercentage'],
-                     data['averageSpeedInKmph'], data['maxSpeedInKmph']))
+        # totalFuelConsumedInL in daily statistics seems to be l/100 km, not total liters.
+        if 'totalFuelConsumedInL' not in data:
+            # If there is not this field in data it means drive was full EV. Set fuel consumed to 0.
+            data['totalFuelConsumedInL'] = 0
+        try:
+            print('{}: EV: {:.1f}/{:.1f} km, {:.0f}%, avg/max speed: {:.0f}/{:.0f} km/h, fuel consumption: {:.1f} l/100 km'.
+                  format(date.format('YYYY-MM-DD'), data['evDistanceInKm'],
+                         data['totalDistanceInKm'], data['evDistancePercentage'],
+                         data['averageSpeedInKmph'], data['maxSpeedInKmph'],
+                         data['totalFuelConsumedInL']))
+        except KeyError:
+            log.warning('No EV driving data!')
+            print('{}: {:.1f} km, avg/max speed: {:.0f}/{:.0f} km/h, fuel consumption: {:.1f} l/100 km'.
+                  format(date.format('YYYY-MM-DD'),
+                         data['totalDistanceInKm'],
+                         data['averageSpeedInKmph'], data['maxSpeedInKmph'],
+                         data['totalFuelConsumedInL']))
 
 
 def parse_weekly_driving_statistics(data):
     """
     Parse weekly statistics
+
+    NOTICE: Week numbers are not ISO week numbers but Japan week numbers! Week is Sunday-Saturday. '
+                'Example: 2021-01-31 is on week 6 instead of ISO week 4!
+
     :param data: statistics data
     :return:
     """
+    log.warning('Week numbers are not ISO week numbers but Japan week numbers! Week is Sunday-Saturday. '
+                'Example: 2021-01-31 is on week 6 instead of ISO week 4!')
     for item in data['histogram']:
         year = item['bucket']['year']
         week = item['bucket']['week']
         date = '{} W{}'.format(year, week)
         data = item['data']
-        print('{}: EV: {:.1f}/{:.1f} km, {:.0f}%, avg speed: {:.0f} km/h, max speed: {:.0f} km/h'.
-              format(date.format('YYYY-MM-DD'), data['evDistanceInKm'],
-                     data['totalDistanceInKm'], data['evDistancePercentage'],
-                     data['averageSpeedInKmph'], data['maxSpeedInKmph']))
+        # totalFuelConsumedInL in weekly statistics seems to be l/100 km, not total liters.
+        if 'totalFuelConsumedInL' not in data:
+            # If there is not this field in data it means drive was full EV. Set fuel consumed to 0.
+            data['totalFuelConsumedInL'] = 0
+        try:
+            print('{}: EV: {:.1f}/{:.1f} km, {:.0f}%, avg/max speed: {:.0f}/{:.0f} km/h, trips/night count: {}/{}, fuel consumption: {:.2f}'.
+                  format(date.format('YYYY-MM-DD'), data['evDistanceInKm'],
+                         data['totalDistanceInKm'], data['evDistancePercentage'],
+                         data['averageSpeedInKmph'], data['maxSpeedInKmph'],
+                         data['tripCount'], data['nightTripsCount'],
+                         data['totalFuelConsumedInL']))
+        except KeyError:
+            log.warning('No EV driving data!')
+            print('{}: {:.1f} km, avg/max speed: {:.0f}/{:.0f} km/h, trips/night count: {}/{}, fuel consumption: {:.2f}'.
+                  format(date.format('YYYY-MM-DD'),
+                         data['totalDistanceInKm'],
+                         data['averageSpeedInKmph'], data['maxSpeedInKmph'],
+                         data['tripCount'], data['nightTripsCount'],
+                         data['totalFuelConsumedInL']))
 
 
 def parse_yearly_driving_statistics(data):
@@ -78,10 +117,20 @@ def parse_yearly_driving_statistics(data):
     :return:
     """
     data = data['summary']
-    print('EV: {:.1f}/{:.1f} km, {:.0f}%, avg speed: {:.0f} km/h, max speed: {:.0f} km/h'.
-          format(data['evDistanceInKm'],
-                 data['totalDistanceInKm'], data['evDistancePercentage'],
-                 data['averageSpeedInKmph'], data['maxSpeedInKmph']))
+    # totalFuelConsumedInL in yearly statistics seems to be l/100 km, not total liters.
+    try:
+        print('EV: {:.1f}/{:.1f} km, {:.0f}%, avg speed: {:.0f} km/h, max speed: {:.0f} km/h, trips/night count: {}/{}, fuel consumption: {:.2f} l/100 km'.
+              format(data['evDistanceInKm'],
+                     data['totalDistanceInKm'], data['evDistancePercentage'],
+                     data['averageSpeedInKmph'], data['maxSpeedInKmph'],
+                     data['tripCount'], data['nightTripsCount'],
+                     data['totalFuelConsumedInL']))
+    except KeyError:
+        print('{:.1f} km, avg/max speed: {:.0f}/{:.0f} km/h, trips/night count: {}/{}, fuel consumption: {:.2f} l/100 km'.
+              format(data['totalDistanceInKm'],
+                     data['averageSpeedInKmph'], data['maxSpeedInKmph'],
+                     data['tripCount'], data['nightTripsCount'],
+                     data['totalFuelConsumedInL']))
 
 
 def main():
@@ -105,7 +154,12 @@ def main():
     if args.from_date:
         from_date = args.from_date
 
-    data, fresh = myt.get_driving_statistics(from_date, interval)
+    try:
+        data, fresh = myt.get_driving_statistics(from_date, interval)
+    except ValueError:
+        log.info('Failed to use cached token, doing fresh login...')
+        myt.login()
+        data, fresh = myt.get_driving_statistics(from_date, interval)
     if args.interval == 'day':
         parse_daily_driving_statistics(myt, data)
     if args.interval == 'week':
